d42e3d3045593601ee430f314e60d5dbcf05fb2a
jt15s <75475674+jt15s@users.noreply.github.com>
2023-12-18 16:47:31 +1100

    Create layout
    Create site layout and transition Start Here page to use the layout. Also enable 11ty to use the favicons located in the root directory.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css')
     eleventyConfig.addPassthroughCopy('img')
+    eleventyConfig.addPassthroughCopy('favicon.ico')
+    eleventyConfig.addPassthroughCopy('favicon.png')
   }
