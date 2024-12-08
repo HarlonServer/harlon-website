fd16b2fe65ee5bfbdb6bfc084a73ce9c69df553a
MuffinBarde <76058321+muffinbarde@users.noreply.github.com>
2024-12-08 20:17:43 +0100

    upgrade to 11ty 3.0.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css')
     eleventyConfig.addPassthroughCopy('img')
     eleventyConfig.addPassthroughCopy('favicon.ico')
     eleventyConfig.addPassthroughCopy('favicon.png')
+    eleventyConfig.addPlugin(UpgradeHelper);
   }
